c61944116a3671e7b97b9366db360cdfc45506ad
Panagiotis <panagiotis---@live.com>
2025-09-24 00:21:07 +0300

    Removed most default css, html code that comes out of the box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -21,16 +19,7 @@ function App() {
   }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
+    <>      
       <p>I've seent it with Vite + React!</p>
       
       <div className="card">
